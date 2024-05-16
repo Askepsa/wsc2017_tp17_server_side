9631b82ae905838e269d497b416c2e787aecdc31
Askepsa <jam.lugtus@gmail.com>
2024-05-16 14:52:07 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn root() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(root)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
